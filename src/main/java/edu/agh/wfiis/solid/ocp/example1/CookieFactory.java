d60dfa3acd8dc438dfe477d44f299bd16152aa36
Piotr Majkut <piotrek.m53@gmail.com>
2019-04-08 14:38:36 +0200

    Update CookieFactory.java

--- src/main/java/edu/agh/wfiis/solid/ocp/example1/CookieFactory.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example1/CookieFactory.java
@@ -3,7 +3,7 @@ package edu.agh.wfiis.solid.ocp.example1;
 import javax.servlet.http.Cookie;
 
 public  class CookieFactory{
-    public cookie create(HeaderType headerType, String value){
+    public Cookie create(HeaderType headerType, String value){
         //choose cookie type and proceed to build method
     }
 }
