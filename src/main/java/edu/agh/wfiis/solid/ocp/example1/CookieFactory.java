48e1a9f5e969d49a791994590f031a72a592dba5
alexsh97 <alexsh997@gmail.com>
2019-04-10 17:41:03 +0200

    Update CookieFactory.java

--- src/main/java/edu/agh/wfiis/solid/ocp/example1/CookieFactory.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example1/CookieFactory.java
@@ -5,27 +5,10 @@ import javax.servlet.http.Cookie;
 public class CookieFactory {
 
     public Cookie create(HeaderType headerType, String value) {
-        switch (headerType) {
-            case SESSION: {
-                Cookie cookie = new Cookie(headerType.name(), value);
-                cookie.setMaxAge(1000);
-                cookie.setSecure(true);
-                cookie.setDomain("security");
-                return cookie;
-            }
-            case ISM: {
-                Cookie cookie = new Cookie(headerType.name(), value);
-                cookie.setDomain("ism");
-                cookie.setSecure(true);
-                return cookie;
-            }
-            default: {
-                Cookie cookie = new Cookie("default", value);
-                cookie.setPath("/applications/" + value);
-                return cookie;
 
+        CookieTypes cookieTypes  = new CookieTypes();
+        CreatableCookieType cookieType = cookieTypes.getCookieTypeCreator(headerType);
 
-            }
-        }
+        return cookieType.create(value);
     }
 }
