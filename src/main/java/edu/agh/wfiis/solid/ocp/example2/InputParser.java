a1b487ecead5d6779875cdf314e363e9bd28c300
gravvy23 <majkutw@gmail.com>
2019-04-23 21:25:02 +0200

    small fix

--- src/main/java/edu/agh/wfiis/solid/ocp/example2/InputParser.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/InputParser.java
@@ -6,7 +6,7 @@ public class InputParser {
 
 		if (data.length != 3) 
 		{
-			throw new IllegalArgumentException("Invalid number of input arguments. 3 expected, " + data.length + " received.");
+			throw new IllegalArgumentException("InputParser: Invalid number of input arguments. 3 expected, " + data.length + " received.");
 		}
 
 		int val1 = Integer.valueOf(data[0]);
