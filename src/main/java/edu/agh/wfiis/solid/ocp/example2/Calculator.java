6704c7d926af0d2ca5b22551e71f41f02187b99c
arokasprz100 <arkadiusz.kasprzak@onet.pl>
2019-04-25 17:43:43 +0200

    add result handler usage

--- src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
@@ -2,17 +2,19 @@ package edu.agh.wfiis.solid.ocp.example2;
 
 public class Calculator {
 
-    public int calculate(String[] inputData) {
+    public int calculate(String[] inputData, HandlingResult resultHandler) {
         MathOperation mathOperation = MathOperationParser.parse(inputData);
 
         Calculating calculation = CalculationChooser.chooseCalculation(mathOperation.getOperator());
         int result = calculation.calculate(mathOperation.getLhsValues(), mathOperation.getRhsValue());
+
+        resultHandler.handleResult(result);
 
         return result;
     }
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.calculate(args);
+        calculator.calculate(args, new ConsolePrinter());
     }
 }
